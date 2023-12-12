69ad37b91dc5fdf662c6914d197f31edba46b4bc
MaFa211 <mats.faulborn@gmx.net>
2023-12-12 18:05:15 +0100

    tutorial 7 notebook

---
+++ Tutorials/tutorial_7/test.py
@@ -0,0 +1,15 @@
+import openai
+
+from openai import OpenAI
+api_base="http://91.107.239.71:80"
+api_key='yAQ09m9ZJQvtQJif9mjF'
+
+
+client =  OpenAI(api_key=api_key, base_url = api_base)
+        
+
+responses = client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": 'Hello ChatGPT!'}],n=1)
+
+print(responses)
+
+
